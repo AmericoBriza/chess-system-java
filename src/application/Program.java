a3513b519d702f30fd4ad33ef3feaa79e90e4659
Americo Briza <americo.briza@gmail.com>
2023-08-31 17:17:04 -0300

    Inicio implementacao classes Piece e Board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,13 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Position posicao = new Position(4, 4);
-		System.out.println(posicao);
+		Board tab = new Board(8, 8);
 
 	}
 
