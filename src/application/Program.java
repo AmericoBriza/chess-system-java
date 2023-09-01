ba104b64cbfbca99faa2cf338d20c4eebde44b8d
Americo Briza <americo.briza@gmail.com>
2023-09-01 12:46:22 -0300

    Chess layer e impressao tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,12 +2,14 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Board tab = new Board(8, 8);
+		ChessMatch partida = new ChessMatch();
+		UI.printBoard(partida.getPieces());
 
 	}
 
