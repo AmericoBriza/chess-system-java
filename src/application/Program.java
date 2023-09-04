c50aee8b18100d2a6829333fa3af2347dbeef913
Americo Briza <americo.briza@gmail.com>
2023-09-04 19:31:20 -0300

    Ajustes tabuleiro / movendo peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,37 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
 
 		ChessMatch partida = new ChessMatch();
-		UI.printBoard(partida.getPieces());
+		
+		while(true) {
+			UI.printBoard(partida.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = partida.performChessMove(source, target);
+			
+		}
+		
+		
+		
 
+	
 	}
 
 }
